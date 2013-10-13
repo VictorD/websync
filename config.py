6d7203e64e7091808f3590f705a97b7c45105119
D7020E <d7020e@eDZ-VirtualBox.(none)>
2013-10-13 18:22:41 +0200

    Added run.py with dynamic port and db-name selection

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
-_basedir = os.path.abspath(os.path.dirname(__file__))
+import sys
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+portdb=str(sys.argv[-1])
 
 DEBUG = False
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
-
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, (portdb + '.db'))
--- run.py
+++ run.py
@@ -1,2 +1,8 @@
-from app import app
-app.run('0.0.0.0', debug=True)
+from app import app, db
+
+if __name__ == '__main__':
+   import sys 
+   port=int(sys.argv[-1])
+   if isinstance(port, int):
+      db.create_all()
+      app.run('0.0.0.0', port=port,debug=True)
