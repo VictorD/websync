5ac0f4c91a6948783c0d7409bac52188642170cb
Lightweight <victor.danell@gmail.com>
2013-10-31 11:58:35 +0100

    i dont get it? timeout much? u think this is a game??

--- nova.py
+++ nova.py
@@ -1,53 +1,70 @@
 #!/usr/bin/env python
 from fabric.api import *
+from fabric.tasks import execute
 from novaclient.v1_1 import client
 import os, time
 
+class FabricSupport:
+    def __init__ (self):
+        pass
+
+    def run(self, host, port, command):
+        env.host_string = "%s:%s" % (host, port)
+        run(command)
+
+myfab = FabricSupport()
+#myfab.run('example.com', 22, 'uname')
+
 #nova --os-username "student-project-9" --os-tenant-id="b0f27ff1c56b4e18a893157d1cfee705" 
 #     --os-auth-url="http://130.240.233.106:5000/v2.0" --os-password="jTKmDEO5Xl5H" image-list
+def createClient():
+   return client.Client(username   = "student-project-9",
+                        project_id = "student-project-9",
+                        api_key    = "jTKmDEO5Xl5H",
+                        auth_url   = "http://130.240.233.106:5000/v2.0")
+                   
+def spawnInstance(c):
+   flavors = c.flavors.list()
+   images  = c.images.list()
+   if flavors and images:
+      instance = c.servers.create(name="Meepo", image=images[0], flavor=flavors[0], key_name="TreasureKey")
 
-def createServer():
-    nv = client.Client(username="student-project-9",
-                   api_key="jTKmDEO5Xl5H",
-                   project_id="student-project-9", 
-                   auth_url="http://130.240.233.106:5000/v2.0")
+      status = instance.status
 
-    flavors = nv.flavors.list()
-    images = nv.images.list()
-    if flavors and images:
-        instance = nv.servers.create(name="Meepo", image=images[0], flavor=flavors[0], key_name="TreasureKey")
+      print "status: %s" % status
+      while status != 'ACTIVE':
+         time.sleep(2)
+         print "."
+         # Update status fields
+         instance = c.servers.get(instance.id)
+         status = instance.status
 
-        status = instance.status
+      print "status: %s" % status
 
-        print "status: %s" % status
-        while status != 'ACTIVE':
-            time.sleep(2)
-            print "."
-            # Update status fields
-            instance = nv.servers.get(instance.id)
-            status = instance.status
-	    print "status: %s" % status
+      # Wait a little to ensure we got a fixed ip (To prevent error: nw_cache missing)
+      time.sleep(1)
+   return instance
 
-        #while not nv.fixed_ips.list():
-        #    time.sleep(1)
-        #    print "waiting for fixed ip"
-        time.sleep(2)
-        ips = nv.floating_ips.list()
-
-        new_floating_ip = nv.floating_ips.create()
+def assignFloatingIP(client, instance):
+        ips = client.floating_ips.list()
+        new_floating_ip = client.floating_ips.create()
         instance.add_floating_ip(new_floating_ip)
-    
-        env.roledefs = {
-            'coreos' : ['core@' + str(new_floating_ip.ip)]
-        }
-        env.key_filename = '/home/vcd/code/websync/TreasureKey.pem'
-        print env.hosts
+        serverIP = str(new_floating_ip.ip)
+        print "Server IP: " + serverIP
+        return serverIP
 
-@roles('coreos')
-def deploy():
-	run('uname -a')
+def setupDocker(ip):
+   env.key_filename = '/home/vcd/code/websync/TreasureKey.pem'
+   env.user = 'core'
+   env.host_string = ip + ":22"
+   run('uname -a')
 
 if __name__ == '__main__':
-    createServer()
-    deploy()
+    client   = createClient()
+    instance = spawnInstance(client)
+    if instance:
+      serverIP = assignFloatingIP(client, instance)
+      time.sleep(2)
+      if serverIP:
+         setupDocker(serverIP)
 
